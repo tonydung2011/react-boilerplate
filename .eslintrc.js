9d0c3eb15b3b4f4dbf8a57902be6321facb256a4
admin <tranngocdung.201194@gmail.com>
2018-10-29 23:31:33 +0700

    update eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -78,6 +78,7 @@ module.exports = {
     'redux-saga/no-yield-in-race': 2,
     'redux-saga/yield-effects': 2,
     'require-yield': 0,
+    'react/no-access-state-in-setstate': 'off',
   },
   settings: {
     'import/resolver': {
