9955a5aa61ce71c1e3998b89d60e26455c558179
admin <tranngocdung.201194@gmail.com>
2018-11-17 18:29:39 +0700

    feature: add more UI in home page

--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -11,9 +11,18 @@ import { Helmet } from 'react-helmet';
 // import { FormattedMessage } from 'react-intl';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+import 'rc-tooltip/assets/bootstrap.css';
 
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import RefreshIcon from '@material-ui/icons/Refresh';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
 import { withStyles } from '@material-ui/core/styles';
 
 import injectSaga from 'utils/injectSaga';
@@ -23,14 +32,36 @@ import reducer from './reducer';
 import saga from './saga';
 // import messages from './messages';
 
+const { createSliderWithTooltip } = Slider;
+const Range = createSliderWithTooltip(Slider.Range);
+
 const styles = () => ({
   input: {
     display: 'none',
+  },
+  icon: {
+    marginTop: 'auto',
   },
 });
 
 /* eslint-disable react/prefer-stateless-function */
 export class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filter: {
+        minPrice: 0,
+        maxPrice: 9999,
+        hero: '',
+        rarity: '',
+      },
+      player: {
+        sort: '',
+        search: '',
+      },
+    };
+  }
+
   renderSelectedItemYou = () => (
     <p>SELECT THE ITEMS YOU WANT TO OFFER FROM THE INVENTORY BOX BELOW</p>
   );
@@ -38,6 +69,26 @@ export class Home extends React.Component {
   renderSelectedItemBot = () => (
     <p>SELECT THE ITEMS YOU WANT TO OFFER FROM THE INVENTORY BOX BELOW</p>
   );
+
+  onChangePrice = field => event => {
+    const value = typeof event === 'object' ? event.target.value : event;
+    this.setState({
+      filter: {
+        ...this.state.filter,
+        [field]: value || 0,
+      },
+    });
+  };
+
+  onChangePlayer = field => event => {
+    const value = typeof event === 'object' ? event.target.value : event;
+    this.setState({
+      player: {
+        ...this.state.player,
+        [field]: value,
+      },
+    });
+  };
 
   render() {
     const { classes } = this.props;
@@ -52,7 +103,7 @@ export class Home extends React.Component {
           <div className="body">
             <Grid container>
               <Grid item sm={5} className="select-area offer">
-                <div>
+                <div id="item-selected-area">
                   <div className="pad-10 select-area-header">
                     <Grid container>
                       <Grid item xs={6}>
@@ -67,6 +118,50 @@ export class Home extends React.Component {
                     {this.renderSelectedItemYou()}
                   </div>
                 </div>
+                <br />
+                <div id="item-list-user">
+                  <div>
+                    <div className="pad-10 select-area-header">
+                      <Grid container>
+                        <Grid item sm={3}>
+                          <RefreshIcon className={classes.icon} />
+                        </Grid>
+                        <Grid item sm={4}>
+                          <FormControl>
+                            <InputLabel htmlFor="age-simple">Order</InputLabel>
+                            <Select
+                              value={this.state.player.sort}
+                              onChange={this.onChangePlayer('sort')}
+                              inputProps={{
+                                name: 'Order',
+                                id: 'order-id',
+                              }}
+                            >
+                              <MenuItem value="price">Price</MenuItem>
+                              <MenuItem value="name">Name</MenuItem>
+                              <MenuItem value="hero">hero</MenuItem>
+                              <MenuItem value="rarity">rarity</MenuItem>
+                            </Select>
+                          </FormControl>
+                        </Grid>
+                        <Grid item sm={5}>
+                          <TextField
+                            id="item-player-search-field"
+                            label="Search"
+                            value={this.state.player.search}
+                            onChange={this.onChangePlayer('search')}
+                            InputLabelProps={{
+                              shrink: true,
+                            }}
+                            margin="normal"
+                            fullWidth
+                          />
+                        </Grid>
+                      </Grid>
+                    </div>
+                    <div className="pad-10 select-area-body" id="items-list" />
+                  </div>
+                </div>
               </Grid>
               <Grid item sm={2}>
                 <div className="toolbar">
@@ -78,6 +173,88 @@ export class Home extends React.Component {
                   >
                     TRADE
                   </Button>
+                </div>
+                <div className="toolbar-filter">
+                  <div className="pad-10 select-area-header">
+                    <p className="text-align-center">BOT FILTER</p>
+                  </div>
+                  <div className="pad-10 select-area-body">
+                    <div id="price-filter">
+                      <p className="text-align-center margin-y-0">PRICE</p>
+                      <Grid container spacing={8}>
+                        <Grid item sm={6}>
+                          <TextField
+                            id="item-filter-min-price"
+                            label="Min"
+                            value={this.state.filter.minPrice}
+                            onChange={this.onChangePrice('minPrice')}
+                            type="number"
+                            className={classes.textField}
+                            InputLabelProps={{
+                              shrink: true,
+                            }}
+                            margin="normal"
+                            fullWidth
+                          />
+                        </Grid>
+                        <Grid item sm={6}>
+                          <TextField
+                            id="item-filter-max-price"
+                            label="Max"
+                            value={this.state.filter.maxPrice}
+                            onChange={this.onChangePrice('maxPrice')}
+                            type="number"
+                            InputLabelProps={{
+                              shrink: true,
+                            }}
+                            margin="normal"
+                            fullWidth
+                          />
+                        </Grid>
+                      </Grid>
+                      <br />
+                      <Range
+                        min={0}
+                        max={9999}
+                        defaultValue={[3, 9999]}
+                        value={[
+                          this.state.filter.minPrice,
+                          this.state.filter.maxPrice,
+                        ]}
+                        onChange={arg => {
+                          this.onChangePrice('minPrice')(arg[0]);
+                          this.onChangePrice('maxPrice')(arg[1]);
+                        }}
+                        tipFormatter={value => `${value}$`}
+                      />
+                    </div>
+                    <div id="hero-filter">
+                      <TextField
+                        id="item-filter-hero"
+                        label="Hero"
+                        value={this.state.filter.hero}
+                        onChange={this.onChangePrice('hero')}
+                        InputLabelProps={{
+                          shrink: true,
+                        }}
+                        margin="normal"
+                        fullWidth
+                      />
+                    </div>
+                    <div id="hero-rarity">
+                      <TextField
+                        id="item-filter-rarity"
+                        label="rarity"
+                        value={this.state.filter.rarity}
+                        onChange={this.onChangePrice('rarity')}
+                        InputLabelProps={{
+                          shrink: true,
+                        }}
+                        margin="normal"
+                        fullWidth
+                      />
+                    </div>
+                  </div>
                 </div>
               </Grid>
               <Grid item sm={5} className="select-area receive">
