041a4e9e1f8efef13cb942d137ce5a67b0d6632f
admin <tranngocdung.201194@gmail.com>
2018-12-21 00:28:53 +0700

    remove unused folder

--- app/containers/DotaItemsAll/tests/reducer.test.js
+++
@@ -1,8 +0,0 @@
-import { fromJS } from 'immutable';
-import dotaItemsAllReducer from '../reducer';
-
-describe('dotaItemsAllReducer', () => {
-  it('returns the initial state', () => {
-    expect(dotaItemsAllReducer(undefined, {})).toEqual(fromJS({}));
-  });
-});
--- app/containers/DotaItemsAll/tests/selectors.test.js
+++
@@ -1,8 +0,0 @@
-// import { fromJS } from 'immutable';
-// import { selectDotaItemsAllDomain } from '../selectors';
-
-describe('selectDotaItemsAllDomain', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
-  });
-});
